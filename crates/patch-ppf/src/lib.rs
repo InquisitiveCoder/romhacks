5b58fdbef870d3adc1a55168790d1829c2870e43
Armando Doval <armando.doval88@gmail.com>
2026-03-03 22:31:11 -0500

    wip

--- crates/patch-ppf/src/lib.rs
+++ crates/patch-ppf/src/lib.rs
@@ -5,20 +5,17 @@ use read_write_utils::prelude::*;
 use result_result_try::try2;
 use rompatcher_err::*;
 use std::borrow::Cow;
-use std::fmt::Formatter;
 use std::hash::Hasher;
 use std::io::prelude::*;
 use std::io::SeekFrom;
-use std::num;
-use std::ops::Range;
+use std::num::{NonZeroU32, NonZeroU8};
+use std::ops::{Deref, DerefMut, Range};
 use std::{cmp, io};
 use PatchingError::*;
 
 pub const MAGIC: &[u8] = b"PPF";
 
 const BLOCK_CHECK_LENGTH: u16 = 1024;
-const BEGIN_MAGIC: &[u8] = b"@BEGIN_FILE_ID.DIZ";
-const END_MAGIC: &[u8] = b"@END_FILE_ID.DIZ";
 
 /// Applies a PPF patch to a ROM.
 pub fn patch(
@@ -27,31 +24,21 @@ pub fn patch(
   output: &mut impl BufWrite,
   strict: bool,
 ) -> io::Result<Result<(), PatchingError>> {
-  let mut patch = PositionTracker::from_start(patch);
+  let mut patch = Patch(PositionTracker::from_start(patch));
+  let mut rom = PositionTracker::from_start(rom);
+  let mut hasher = crc32fast::Hasher::new();
+
   let Format {
     block_check,
     footer_body_len_size,
     has_undo_data,
-    rom_offset_type,
-  } = try2!(Format::parse_and_validate(&mut patch)?);
-  let mut rom = PositionTracker::from_start(rom);
-
-  let offset_size: usize = rom_offset_type.size();
-  let mut hasher = crc32fast::Hasher::new();
+    offset_size,
+  } = try2!(patch.parse_format()?);
 
   loop {
-    let offset: u64 = {
-      let mut buf = [0u8; size_of::<u64>()];
-      u64::from_le_bytes(try2!(
-        patch
-          .read_exact(&mut buf[..offset_size])
-          .map(|_| buf)
-          .map_patch_err()?
-      ))
-    };
-
+    let offset: u64 = try2!(patch.read_offset(offset_size)?);
     let hunk_length: u8 = try2!(patch.read_u8().map_patch_err()?);
-    try2!(num::NonZeroU8::new(hunk_length).ok_or(BadPatch));
+    let hunk_length: NonZeroU8 = try2!(NonZeroU8::new(hunk_length).ok_or(BadPatch));
 
     // If the patch includes a block check, we need to compare a specific 1 KB
     // block of the ROM to the block that was included in the patch header.
@@ -108,15 +95,15 @@ pub fn patch(
     debug_assert_eq!(rom.position(), offset);
     try2!(
       patch
-        .copy_exactly(u64::from(hunk_length), output)
+        .copy_exactly(u64::from(hunk_length.get()), output)
         .map_patch_err()?
     );
 
     if has_undo_data {
-      patch.seek_relative(hunk_length.into())?;
+      patch.seek_relative(i64::from(hunk_length.get()))?;
     }
 
-    if try2!(is_end_of_patch(&mut patch, footer_body_len_size)?) {
+    if try2!(patch.has_reached_end(footer_body_len_size)?) {
       break;
     }
   }
@@ -128,16 +115,9 @@ pub fn patch(
   Ok(Ok(()))
 }
 
-/// Details about the format of a PPF file.
-#[derive(Clone, Debug, PartialEq, Eq, Hash)]
-struct Format {
-  rom_offset_type: RomOffsetType,
-  footer_body_len_size: Option<FooterBodyLengthType>,
-  has_undo_data: bool,
-  block_check: Option<BlockCheck>,
-}
+struct Patch<T>(T);
 
-impl Format {
+impl<T: BufRead + Seek> Patch<PositionTracker<&mut T>> {
   /// Parses the PPF header and footer and performs block check validation.
   ///
   /// `patch`'s cursor must be at the start of the file, and `eof` must be the
@@ -145,17 +125,14 @@ impl Format {
   ///
   /// If this method returns `Ok`, `patch` will be positioned at the start of
   /// the patch data. No guarantees are made about its cursor position otherwise.
-  pub fn parse_and_validate(
-    patch: &mut PositionTracker<impl BufRead + Seek>,
-  ) -> io::Result<Result<Format, PatchingError>> {
+  pub fn parse_format(&mut self) -> io::Result<Result<Format, PatchingError>> {
     // applyppf3 parses the magic string to obtain the version number and
     // ignores the dedicated version byte. However, ROM Patcher JS checks both
     // and throws an error if they don't match. Given the latter's widespread
     // use, it's probably safe to follow its lead.
-    let version = try2!(Version::try_from(
-      &(try2!(patch.read_array::<5>().map_patch_err()?))
-    ));
-    if version != try2!(Version::try_from(try2!(patch.read_u8().map_patch_err()?))) {
+    let version_string = try2!(self.read_version_string()?);
+    let version_byte = try2!(self.read_version_byte()?);
+    if version_string != version_byte {
       return Ok(Err(BadPatch));
     }
 
@@ -165,15 +142,15 @@ impl Format {
     // String::from_utf8_lossy will cast the byte slice without having to copy
     // and modify the string, while str::trim_end will handle trailing spaces.
     // Nul bytes aren't displayed even if they're in the middle of a string.
-    let description: [u8; 50] = patch.read_array()?;
+    let description: [u8; 50] = self.read_array()?;
     let description: Cow<str> = String::from_utf8_lossy(&description);
     let description: &str = description.trim_end();
     log::debug!("PPF patch description: {description}");
 
-    Ok(Ok(match version {
+    Ok(Ok(match version_string {
       Version::V1 => Format {
         footer_body_len_size: None,
-        rom_offset_type: RomOffsetType::U32,
+        offset_size: NonZeroU8::new(4).unwrap(),
         has_undo_data: false,
         block_check: None,
       },
@@ -181,126 +158,190 @@ impl Format {
         // File size checks were deprecated in V3 because they were unreliable,
         // but an absent file size might indicate an invalid PPF file.
         try2!(
-          num::NonZeroU32::try_from(try2!(patch.read_u32::<LE>().map_patch_err()?))
-            .map_err(|_| BadPatch)
+          NonZeroU32::try_from(try2!(self.read_u32::<LE>().map_patch_err()?)).map_err(|_| BadPatch)
         );
-        let mut hashing_writer = HashingWriter::new(io::sink(), crc32fast::Hasher::new());
-        patch.take_from_inner(BLOCK_CHECK_LENGTH as u64, |take| {
-          take.exactly(|patch| io::copy(patch, &mut hashing_writer))
-        })?;
-        let block_start = u64::from(ImageType::BIN.block_check_offset());
-        let block_end = block_start + u64::from(BLOCK_CHECK_LENGTH);
+        let crc32 = try2!(self.hash_validation_block()?);
         Format {
-          footer_body_len_size: Some(FooterBodyLengthType::U32),
-          rom_offset_type: RomOffsetType::U32,
+          footer_body_len_size: NonZeroU8::new(4),
+          offset_size: NonZeroU8::new(4).unwrap(),
           has_undo_data: false,
-          block_check: Some(BlockCheck {
-            region: CheckedRange::new(block_start..block_end).unwrap(),
-            crc32: hashing_writer.into_hasher().finalize(),
-          }),
+          block_check: Some(BlockCheck { crc32, region: ImageType::BIN.block_check_range() }),
         }
       }
       Version::V3 => {
-        let image_type = try2!(ImageType::try_from(try2!(patch.read_u8().map_patch_err()?)));
+        let image_type = try2!(self.read_image_type()?);
         let has_block_check = try2!(
-          try2!(patch.read_u8().map_patch_err()?)
+          try2!(self.read_u8().map_patch_err()?)
             .try_into_bool()
             .map_err(|_| BadPatch)
         );
         let has_undo_data = try2!(
-          try2!(patch.read_u8().map_patch_err()?)
+          try2!(self.read_u8().map_patch_err()?)
             .try_into_bool()
             .map_err(|_| BadPatch)
         );
-        patch.seek_relative(1)?; // Unused in V3
+        self.seek_relative(1)?; // Unused in V3
         let block_check = match has_block_check {
           false => None,
           true => {
-            let mut hashing_writer = HashingWriter::new(io::sink(), crc32fast::Hasher::new());
-            patch.take_from_inner(u64::from(BLOCK_CHECK_LENGTH), |take| {
-              take.exactly(|patch| io::copy(patch, &mut hashing_writer))
-            })?;
-            let block_start = u64::from(image_type.block_check_offset());
-            let block_end = block_start + u64::from(BLOCK_CHECK_LENGTH);
-            Some(BlockCheck {
-              region: CheckedRange::new(block_start..block_end).unwrap(),
-              crc32: hashing_writer.into_hasher().finalize(),
-            })
+            let crc32 = try2!(self.hash_validation_block()?);
+            let region = image_type.block_check_range();
+            Some(BlockCheck { crc32, region })
           }
         };
         Format {
-          footer_body_len_size: Some(FooterBodyLengthType::U16),
-          rom_offset_type: RomOffsetType::U64,
+          footer_body_len_size: NonZeroU8::new(2),
+          offset_size: NonZeroU8::new(8).unwrap(),
           has_undo_data,
           block_check,
         }
       }
     }))
   }
+
+  /// Parse the end of the PPF2 or PPF3 patch data. PPF2 and PPF3 files may
+  /// have an **optional** footer with the following structure:
+  ///
+  /// `"@BEGIN_FILE_ID.DIZ" BODY "@END_FILE_ID.DIZ" BODY_LENGTH`
+  ///
+  /// where BODY_LENGTH cannot exceed 3072. BODY_LENGTH is 4 bytes long in
+  /// PPF2 files and 2 bytes long in PPF3. (It's not clear why 4 bytes were
+  /// reserved for this purpose when 2 would suffice; the PPF3 docs don't say.)
+  ///
+  /// The PPF3 documentation refers to the BODY as a file_id, FILE_ID.DIZ, or
+  /// FILE_ID.DIZ file. Because of this lack of consistency and the potential
+  /// ambiguity with the term "file_id area", this code uses the terms "footer"
+  /// and "body" instead.
+  fn has_reached_end(
+    &mut self,
+    footer_body_len_size: Option<NonZeroU8>,
+  ) -> io::Result<Result<bool, PatchingError>> {
+    const BEGIN_MAGIC: &[u8] = b"@BEGIN_FILE_ID.DIZ";
+    const END_MAGIC: &[u8] = b"@END_FILE_ID.DIZ";
+    const MAX_BODY_LEN: u64 = 3072;
+
+    if self.has_reached_eof()? {
+      // No footer.
+      return Ok(Ok(true));
+    }
+
+    let body_len_size = match footer_body_len_size {
+      None => return Ok(Ok(false)), // The patch can't have a footer.
+      Some(footer_body_len_size) => footer_body_len_size,
+    };
+
+    if !self.next_bytes_eq::<{ BEGIN_MAGIC.len() }>(BEGIN_MAGIC)? {
+      // The patch may have a footer, but it hasn't been reached.
+      return Ok(Ok(false));
+    }
+
+    // The start of the footer was found.
+    // Validate the body length and the magic string terminating the footer.
+    let body_start = self.position() + BEGIN_MAGIC.len() as u64;
+    let body_end = self.seek(SeekFrom::End(
+      -(END_MAGIC.len() as i64 + i64::from(body_len_size.get())),
+    ))?;
+    let body_len = try2!(u64::checked_sub(body_end, body_start).ok_or(BadPatch));
+    if body_len > MAX_BODY_LEN {
+      return Ok(Err(BadPatch));
+    }
+    let body_len = body_len as u16; // safe because of the previous check
+    if &self.read_array::<{ END_MAGIC.len() }>()?[..] != END_MAGIC {
+      return Ok(Err(BadPatch));
+    }
+    let expected_body_len: u32 = {
+      // Little endian order yields the same numerical value at larger sizes,
+      // so a 4 byte buffer can be used for both a body_len_size of 2 and 4.
+      let mut buf = [0u8; size_of::<u32>()];
+      self.read_exact(&mut buf[..usize::from(body_len_size.get())])?;
+      u32::from_le_bytes(buf)
+    };
+    if expected_body_len != u32::from(body_len) {
+      return Ok(Err(BadPatch));
+    }
+
+    if !self.has_reached_eof()? {
+      return Ok(Err(BadPatch));
+    }
+
+    Ok(Ok(true))
+  }
 }
 
-/// Parse the end of the PPF2 or PPF3 patch data. PPF2 and PPF3 files may
-/// have an **optional** footer with the following structure:
-///
-/// `"@BEGIN_FILE_ID.DIZ" BODY "@END_FILE_ID.DIZ" BODY_LENGTH`
-///
-/// where BODY_LENGTH cannot exceed 3072. BODY_LENGTH is 4 bytes long in
-/// PPF2 files and 2 bytes long in PPF3. (It's not clear why 4 bytes were
-/// reserved for this purpose when 2 would suffice; the PPF3 docs don't say.)
-///
-/// The PPF3 documentation refers to the BODY as a file_id, FILE_ID.DIZ, or
-/// FILE_ID.DIZ file. Because of this lack of consistency and the potential
-/// ambiguity with the term "file_id area", this code uses the terms "footer"
-/// and "body" instead.
-fn is_end_of_patch(
-  patch: &mut PositionTracker<&mut (impl BufRead + Seek)>,
-  footer_body_len_size: Option<FooterBodyLengthType>,
-) -> io::Result<Result<bool, PatchingError>> {
-  const MAX_BODY_LEN: u64 = 3072;
-
-  if patch.has_reached_eof()? {
-    // No footer.
-    return Ok(Ok(true));
+impl<T: Read> Patch<PositionTracker<&mut T>> {
+  fn hash_validation_block(&mut self) -> io::Result<Result<u32, PatchingError>> {
+    let mut hashing_writer = HashingWriter::new(io::sink(), crc32fast::Hasher::new());
+    try2!(
+      self
+        .copy_exactly(u64::from(BLOCK_CHECK_LENGTH), &mut hashing_writer)
+        .map_patch_err()?
+    );
+    Ok(Ok(hashing_writer.into_hasher().finalize()))
   }
+}
 
-  let body_len_size = match footer_body_len_size.map(FooterBodyLengthType::size) {
-    None => return Ok(Ok(false)), // The patch can't have a footer.
-    Some(footer_body_len_size) => footer_body_len_size,
-  };
-
-  if !patch.next_bytes_eq::<{ BEGIN_MAGIC.len() }>(BEGIN_MAGIC)? {
-    // The patch may have a footer, but it hasn't been reached.
-    return Ok(Ok(false));
+impl<T: BufRead> Patch<T> {
+  fn read_offset(&mut self, size: NonZeroU8) -> io::Result<Result<u64, PatchingError>> {
+    let size = usize::from(size.get());
+    let mut buf = [0u8; size_of::<u64>()];
+    self
+      .read_exact(&mut buf[..size])
+      .map(|_| buf)
+      .map(u64::from_le_bytes)
+      .map_patch_err()
   }
 
-  // The start of the footer was found.
-  // Validate the body length and the magic string terminating the footer.
-  let body_start = patch.position() + BEGIN_MAGIC.len() as u64;
-  let body_end = patch.seek(SeekFrom::End(
-    -(END_MAGIC.len() as i64 + body_len_size as i64),
-  ))?;
-  let body_len = try2!(u64::checked_sub(body_end, body_start).ok_or(BadPatch));
-  if body_len > MAX_BODY_LEN {
-    return Ok(Err(BadPatch));
-  }
-  if &patch.read_array::<{ END_MAGIC.len() }>()?[..] != END_MAGIC {
-    return Ok(Err(BadPatch));
-  }
-  let expected_body_len: u32 = {
-    // Little endian order yields the same numerical value at larger sizes,
-    // so a 4 byte buffer can be used for both a body_len_size of 2 and 4.
-    let mut buf = [0u8; size_of::<u32>()];
-    patch.read_exact(&mut buf[..body_len_size])?;
-    u32::from_le_bytes(buf)
-  };
-  if expected_body_len != (body_len as u32) {
-    return Ok(Err(BadPatch));
-  }
-  if !patch.has_reached_eof()? {
-    return Ok(Err(BadPatch));
+  fn read_version_string(&mut self) -> io::Result<Result<Version, PatchingError>> {
+    let bytes = try2!(self.read_array::<5>().map_patch_err()?);
+    match bytes.as_slice() {
+      b"PPF10" => Ok(Ok(Version::V1)),
+      b"PPF20" => Ok(Ok(Version::V2)),
+      b"PPF30" => Ok(Ok(Version::V3)),
+      _ => Ok(Err(BadPatch)),
+    }
   }
 
-  Ok(Ok(true))
+  fn read_version_byte(&mut self) -> io::Result<Result<Version, PatchingError>> {
+    let value = try2!(self.read_u8().map_patch_err()?);
+    match value {
+      0 => Ok(Ok(Version::V1)),
+      1 => Ok(Ok(Version::V2)),
+      2 => Ok(Ok(Version::V3)),
+      _ => Ok(Err(BadPatch)),
+    }
+  }
+
+  fn read_image_type(&mut self) -> io::Result<Result<ImageType, PatchingError>> {
+    let value = try2!(self.read_u8().map_patch_err()?);
+    match value {
+      0 => Ok(Ok(ImageType::BIN)),
+      1 => Ok(Ok(ImageType::GI)),
+      _ => Ok(Err(BadPatch)),
+    }
+  }
+}
+
+impl<T> Deref for Patch<T> {
+  type Target = T;
+
+  fn deref(&self) -> &Self::Target {
+    &self.0
+  }
+}
+
+impl<T> DerefMut for Patch<T> {
+  fn deref_mut(&mut self) -> &mut Self::Target {
+    &mut self.0
+  }
+}
+
+/// Details about the format of a PPF file.
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+struct Format {
+  offset_size: NonZeroU8,
+  footer_body_len_size: Option<NonZeroU8>,
+  has_undo_data: bool,
+  block_check: Option<BlockCheck>,
 }
 
 /// A PPF2 or PPF3 block check.
@@ -308,23 +349,6 @@ fn is_end_of_patch(
 pub struct BlockCheck {
   region: CheckedRange<Range<u64>, u64>,
   crc32: u32,
-}
-
-impl BlockCheck {
-  pub fn validate(
-    &self,
-    patch: &mut impl Read,
-    file: &mut (impl Read + Seek),
-  ) -> io::Result<Result<(), PatchingError>> {
-    file.seek(io::SeekFrom::Start(self.region.start.into()))?;
-    let file_block: [u8; BLOCK_CHECK_LENGTH as usize] = try2!(file.read_array().map_rom_err()?);
-    let validation_block: [u8; BLOCK_CHECK_LENGTH as usize] =
-      try2!(patch.read_array().map_patch_err()?);
-    if file_block != validation_block {
-      return Ok(Err(BadPatch));
-    }
-    Ok(Ok(()))
-  }
 }
 
 /// The PPF format versions.
@@ -336,42 +360,6 @@ pub enum Version {
   V3,
 }
 
-impl std::fmt::Display for Version {
-  fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-    match self {
-      Version::V1 => write!(f, "PPF1.0"),
-      Version::V2 => write!(f, "PPF2.0"),
-      Version::V3 => write!(f, "PPF3.0"),
-    }
-  }
-}
-
-impl TryFrom<&[u8; 5]> for Version {
-  type Error = PatchingError;
-
-  fn try_from(value: &[u8; 5]) -> Result<Self, Self::Error> {
-    match value {
-      b"PPF10" => Ok(Version::V1),
-      b"PPF20" => Ok(Version::V2),
-      b"PPF30" => Ok(Version::V3),
-      _ => Err(BadPatch),
-    }
-  }
-}
-
-impl TryFrom<u8> for Version {
-  type Error = PatchingError;
-
-  fn try_from(value: u8) -> Result<Self, Self::Error> {
-    match value {
-      0 => Ok(Version::V1),
-      1 => Ok(Version::V2),
-      2 => Ok(Version::V3),
-      _ => Err(BadPatch),
-    }
-  }
-}
-
 /// The ROM image types used in block checks.
 #[derive(Clone, Copy, Debug, Default, PartialOrd, Ord, PartialEq, Eq, Hash)]
 pub enum ImageType {
@@ -381,54 +369,12 @@ pub enum ImageType {
 }
 
 impl ImageType {
-  pub fn block_check_offset(&self) -> u16 {
+  pub fn block_check_range(&self) -> CheckedRange<Range<u64>, u64> {
     match self {
-      ImageType::BIN => 0x9320,
-      ImageType::GI => 0x80A0,
-    }
-  }
-}
-
-impl TryFrom<u8> for ImageType {
-  type Error = PatchingError;
-
-  fn try_from(value: u8) -> Result<Self, Self::Error> {
-    match value {
-      0 => Ok(Self::BIN),
-      1 => Ok(Self::GI),
-      _ => Err(BadPatch),
-    }
-  }
-}
-
-#[derive(Clone, Copy, Debug, Default, PartialOrd, Ord, PartialEq, Eq, Hash)]
-pub enum FooterBodyLengthType {
-  #[default]
-  U16,
-  U32,
-}
-
-impl FooterBodyLengthType {
-  const fn size(self) -> usize {
-    match self {
-      Self::U16 => size_of::<u16>(),
-      Self::U32 => size_of::<u32>(),
-    }
-  }
-}
-
-#[derive(Clone, Copy, Debug, Default, PartialOrd, Ord, PartialEq, Eq, Hash)]
-pub enum RomOffsetType {
-  #[default]
-  U32,
-  U64,
-}
-
-impl RomOffsetType {
-  const fn size(self) -> usize {
-    match self {
-      Self::U32 => size_of::<u32>(),
-      Self::U64 => size_of::<u64>(),
+      ImageType::BIN => {
+        CheckedRange::new(0x9320..(0x9320 + u64::from(BLOCK_CHECK_LENGTH))).unwrap()
+      }
+      ImageType::GI => CheckedRange::new(0x80A0..(0x80A0 + u64::from(BLOCK_CHECK_LENGTH))).unwrap(),
     }
   }
 }
